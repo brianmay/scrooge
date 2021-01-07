2efe91686af574ff4270bd847dcb55bf08e36029
Brian May <brian@linuxpenguins.xyz>
2021-01-07 17:19:35 +1100

    Replace --watch-stdin with --watch

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :scrooge, ScroogeWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
