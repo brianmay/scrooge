b12a43790b3405712aa0befad3d05489a4b54bec
Brian May <brian@linuxpenguins.xyz>
2021-06-08 13:34:29 +1000

    Synchronise with other repos

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule Scrooge.MixProject do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test --no-start"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test --no-start"],
+      prettier: "cmd ./assets/node_modules/.bin/prettier --check . --color"
     ]
   end
 
