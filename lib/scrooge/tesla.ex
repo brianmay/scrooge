7a4f6b201ce3fd3690a82129be43cf79544cf871
Brian May <brian@linuxpenguins.xyz>
2021-06-24 14:29:56 +1000

    Don't fail if old_conditions hasn't been set yet (2)

--- lib/scrooge/tesla.ex
+++ lib/scrooge/tesla.ex
@@ -263,6 +263,8 @@ defmodule Scrooge.Tesla do
   end
 
   @spec check_rule(Rule.t(), {boolean(), any()}, {boolean(), any()}, boolean()) :: :ok
+  def check_rule(rule, nil, {true, new_data}, alert), do: :ok
+
   def check_rule(rule, {old_test, old_data}, {true, new_data}, alert) do
     cond do
       old_test == false -> publish_msg(rule.on_msg, old_data, new_data)
