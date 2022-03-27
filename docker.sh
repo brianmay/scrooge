1ce9962be93f87065643fe29fd727d1a95e90bef
Brian May <brian@linuxpenguins.xyz>
2022-03-27 17:10:47 +1100

    Rename MQTT_USER_NAME to MQTT_USERNAME

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@
       -e MQTT_HOST="$MQTT_HOST" \
       -e MQTT_PORT="$MQTT_PORT" \
       -e MQTT_CA_CERT_FILE="$MQTT_CA_CERT_FILE" \
-      -e MQTT_USER_NAME="$MQTT_USER_NAME" \
+      -e MQTT_USERNAME="$MQTT_USERNAME" \
       -e MQTT_PASSWORD="$MQTT_PASSWORD" \
       -v /etc/ssl/certs:/etc/ssl/certs \
       -v /usr/share/ca-certificates:/usr/share/ca-certificates \
